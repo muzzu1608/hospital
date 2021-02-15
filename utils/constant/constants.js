bc2755ad5443c2b68a62881944a353f5fe1e645d
Muzammil <muzzu1608@gmail.com>
2021-02-15 23:11:26 +0530

    second-commit

---
+++ utils/constant/constants.js
@@ -0,0 +1,12 @@
+export const CONSTANTS = {
+    MAXIMUM_DISH_IMAGES: 6,
+    MAXIMUM_DISH_IMAGES_ERROR_MESSAGE: `Maximum product image is ${this.MAXIMUM_DISH_IMAGES}`,
+    STATIC_SOURCE_FILES: 'staticFiles',
+    STATIC_DESTINATION_FILES: 'public',
+    REMOVE_FOLDER: 'public/images',
+    DISH_IMAGE_PATH: './public/images/dishes/',
+    CATEGORY_DEFAULT_IMAGE: 'public/images/categories/default_category.png',
+    MAXIMUM_CATEGORY_IMAGE_ERROR_MESSAGE: 'Maximum category image is 1',
+    DISH_IMAGE_FOLDER_SEQ_GEN: 'dish',
+    ORDER_NUMBER_SEQ_GEN: 'order',
+};
